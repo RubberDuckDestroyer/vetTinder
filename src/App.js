af05e0124ee5b451a77169e35d32658a939be922
Hitarth Asrani <hitarthasrani@gmail.com>
2021-05-28 13:18:39 +1200

    Added textbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TextField} from "@material-ui/core"
+
 
 function App() {
+
+  const [location, setLocation] = React.useState('');
+
+
+  const updateLocation = (event) => {
+    setLocation(event.target.value)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="location-filter">
+
+        <span>Finding vets around <TextField value={location} onChange={updateLocation} placeholder="Your Location"/></span>
+      </div>
     </div>
   );
 }
