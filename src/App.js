a0bd34b37d878d65f19a3681ddc03498b65a15af
michelle-wdcc <michelle.hoang.102@gmail.com>
2021-05-28 14:07:15 +1200

    Add Card

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {TextField} from "@material-ui/core"
-
+import {TextField, Typography} from "@material-ui/core"
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
 
 function App() {
 
@@ -19,6 +22,24 @@ function App() {
       <div className="location-filter">
 
         <span>Finding vets around <TextField value={location} onChange={updateLocation} placeholder="Your Location"/></span>
+      </div>
+      <div className="vet-card">
+        <Card className={"vet-clinic"}>
+          <CardHeader 
+            title = "Name"
+            subheader="[add sth here]"
+          />
+          <CardMedia 
+            className={"media"}
+            image="./img/vet-dog.jpg"
+            title= "Vet example"
+            />
+          <CardContent>
+            <Typography className="textSecondary" component="p">
+              Clinic location
+            </Typography>
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
