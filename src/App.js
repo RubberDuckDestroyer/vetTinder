c5930cb876612aa93837aedcf33f47073d3f7543
michelle-wdcc <michelle.hoang.102@gmail.com>
2021-05-29 11:07:41 +1200

    Edit design

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CancelIcon from '@material-ui/icons/Cancel';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import profileData from "./data/profiles.json"
 import vetData from "./data/vets.json"
+import * from "react-google-maps"
 
 function App() {
+  const [currentCardIndex, setCardIndex] = React.useState(0);
 
   const [location, setLocation] = React.useState('');
 
@@ -20,15 +23,25 @@ function App() {
     setLocation(event.target.value)
   }
   
-  const [expanded,setExpaned] = React.useState(false);
+  const [expanded,setExpanded] = React.useState(false);
 
   const handleExpandClick = () => {
-    setExpaned(!expanded);
+    setExpanded(!expanded);
   };
 
-  // const profileData = import('');
-  console.log(profileData);
+  const handleClickNo = () => {
 
+    vetData.forEach( (vet) => {
+      vet.image = "img/vet-dog.jpg";
+    })
+
+      if (currentCardIndex == vetData.length - 1) {
+        alert("No more card")
+        setCardIndex(0)
+      }    else { setCardIndex(currentCardIndex+1);}    
+  }
+
+  // const profileData = import('');
   // const vetProfile = JSON.parse(profileData);
 
 
@@ -37,56 +50,76 @@ function App() {
       
       <div className="location-filter">
 
-        <span>Finding vets around <TextField value={location} onChange={updateLocation} placeholder="Your Location"/></span>
+        <span>Finding vets around <TextField value={location} className="textfield" onChange={updateLocation} placeholder="Your Location"/></span>
+
+        <hr></hr>
+
       </div>
       <div className="vet-card">
-        <Card className="vet-clinic">
+      {vetData.slice(currentCardIndex,currentCardIndex+1).map((vet,key) => (
+        <Card >
           <CardHeader 
-            title = "Name"
+          className="vet-clinic"
+            title = {vet.name}
             subheader="[add sth here]"
           />
-          <CardMedia 
+          <div className="media-frame">
+          {/* <CardMedia 
             className="media"
-            image="img/vet-dog.jpg"
+            image={vet.profile}
             title= "Vet example"
-            />
-          <CardContent> 
+            /> */}
+            
+            <GoogleMap
+            defaultZoom={8}
+            defaultCenter={{ lat: -34.397, lng: 150.644 }}/>
+              
+          </div>
+          <CardContent className="place"> 
             <Typography className="textSecondary" component="p">
               Clinic location
             </Typography>
-            <List>
-                {vetData.slice(1,2).map((vet,key) => (
-                <ListItem component="nav" className="vet-local" aria-label="Clinic profile">
+            
+            <Typography component="p" className="vet-local" aria-label="Clinic profile">
                     {vet.vicinity}
-                  </ListItem>
-                ))} 
-                </List>
+                </Typography>
+                
+            
           </CardContent>
+          <div className="icon">
           <CardActions disableSpacing className="actions">
-            <IconButton aria-label="No">
+            <IconButton className="no" aria-label="No" onClick={handleClickNo}>
               <CancelIcon/>
             </IconButton>
-            <IconButton classNames="rightBut"
+            <IconButton classNames="more"
             onClick={handleExpandClick}
             aria-expanded={expanded}
             aria-label="Yes, show vets"
             >
               <ExpandMoreIcon />
-              </IconButton> 
+            </IconButton>
+            <IconButton className="yes" aria-label="Yes">
+              <FavoriteIcon/>
+            </IconButton> 
             </CardActions>
+            
             <Collapse in={expanded} timeout="auto" unmountOnExit>
               <CardContent>
               <List>
                 {profileData.map((profile,key) => (
+                  
                 <ListItem component="nav" className="vet-table" aria-label="Vet profile">
                     {profile.name.first}
+                    
                   </ListItem>
                 ))} 
               </List>
                 
               </CardContent>
             </Collapse>
+            </div>
       </Card>
+      ))} 
       </div>
     </div>
   );
